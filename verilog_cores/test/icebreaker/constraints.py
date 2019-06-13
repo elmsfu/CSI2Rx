16b12c04779e33618c606f1b285ff7e589929bd6
Elms <elms@freshred.net>
2019-06-13 16:21:11 -0700

    trying to pass timing without debug

--- verilog_cores/test/icebreaker/constraints.py
+++ verilog_cores/test/icebreaker/constraints.py
@@ -1,4 +1,3 @@
-ctx.addClock("csi_rx_i.dphy_clk", 96)
+ctx.addClock("csi_rx_i.dbg_dphy_clk", 96)
 ctx.addClock("video_clk", 24)
-ctx.addClock("uart_i.sys_clk_i", 12)
-ctx.addClock("dbg_video_clk_$glb_clk", 24)
+ctx.addClock("clk12", 12)
